43d67836a56e3a56edd93defec24c0f1d0ed7020
SubhoBasak <subhobasak50@email.com>
2025-11-03 21:55:06 +0530

    init commit

---
+++ player.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+@export_group("Player")
+@export var speed: int = 100
+
+@onready var player_image: AnimatedSprite2D = $PlayerImage as AnimatedSprite2D
+
+var screen_size: Vector2
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+func _process(delta: float) -> void:
+	var direction: Vector2 = Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
+	).normalized()
+
+	position += direction * speed * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+	if direction.length() > 0:
+		player_image.play()
+	else:
+		player_image.stop()
+
+	if direction.x != 0:
+		player_image.animation = "walk"
+		player_image.flip_h = direction.x < 0
+		player_image.flip_v = false
+	elif direction.y != 0:
+		player_image.animation = "up"
+		player_image.flip_v = direction.y > 0
+		player_image.flip_h = false
